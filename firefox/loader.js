f400d14288a68df9ddcac097fac7d362f043235b
Adrian Freund <freundTech@gmail.com>
2016-01-03 13:57:59 +0100

    Made firefox extension work on lokal files

--- firefox/loader.js
+++ firefox/loader.js
@@ -2,7 +2,7 @@ var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
-	include: "*",
+	include: ["*", "file://*"],
 	exclude: "*.supermariomakerbookmark.nintendo.net",
 	contentScriptWhen: "ready",
 	contentScriptFile: data.url("mariomaker.js")
