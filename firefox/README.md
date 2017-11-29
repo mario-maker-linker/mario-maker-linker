fd6b261f36d610c888279a5a227973282633403a
Adrian Freund <git@freundtech.com>
2017-11-29 18:02:11 +0100

    Remove legacy firefox extension and update chrome extension to be also compatible with firefox

--- firefox/README.md
+++
@@ -1,2 +0,0 @@
-#Super Mario Maker Course ID Linkifier
-Converts all Super Mario Maker Course IDs into Bookmark links
--- firefox/loader.js
+++
@@ -1,9 +0,0 @@
-var data = require("sdk/self").data;
-var pageMod = require("sdk/page-mod");
-
-pageMod.PageMod({
-	include: ["*", "file://*"],
-	exclude: "*.supermariomakerbookmark.nintendo.net",
-	contentScriptWhen: "ready",
-	contentScriptFile: data.url("mariomaker.js")
-});
